729d4853796de83e0e35c409dea87ee31e6c0345
Pablo El Maldy <pablojamiro2008@hotmail.com>
2024-10-06 12:10:12 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+
+import mdx from '@astrojs/mdx'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx()],
+  site: 'http://example.com'
+})
